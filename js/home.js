fbfb2df5f8523fbc926aa035eb2d64e19e1edf77
/Faisalahammed003 <faisalahammed5035@gmail.com>
2024-09-25 02:13:56 +0600

    full project completed

--- js/home.js
+++ js/home.js
@@ -1,6 +1,7 @@
-// link home to blog
+// link home to blog start
 const HomeButton = document
   .getElementById("home-btn")
   .addEventListener("click", function () {
     window.location.href = "/index.html";
   });
+// link home to blog End
